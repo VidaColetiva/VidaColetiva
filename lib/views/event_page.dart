9e40ad98b1382955809ee8f4717b45b599bee697
Victor Sant'Ana <vgsantanasilva@id.uff.br>
2024-12-10 13:29:06 -0300

    icons generated

--- lib/views/event_page.dart
+++ lib/views/event_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:vidacoletiva/controllers/project_controller.dart';
 import 'package:vidacoletiva/data/models/event_model.dart';
 import 'package:vidacoletiva/resources/assets/colour_pallete.dart';
 
@@ -9,6 +11,9 @@ class EventPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ProjectController projectController =
+        Provider.of<ProjectController>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(event.title ?? ""),
@@ -22,6 +27,40 @@ class EventPage extends StatelessWidget {
           },
         ),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(event.title ?? "", style: const TextStyle(fontSize: 24),),
+            Text(event.description ?? ""),
+            SizedBox(
+              height: 300,
+              width: MediaQuery.of(context).size.width,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  ...photoList(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  Iterable<Widget> photoList() {
+    return event.mediaModelList!.map((media) {
+      return FutureBuilder<String>(
+        future: media.getUrl(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Image.network(snapshot.data.toString(), fit: BoxFit.fitWidth,);
+          } else {
+            return const CircularProgressIndicator();
+          }
+        },
+      );
+    });
   }
 }
